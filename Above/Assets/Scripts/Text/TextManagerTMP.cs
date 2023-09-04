0a7afff8ec44e29ec1435e451a4c234c4b26f068
NeuronStud1o <122213370+NeuronStud1o@users.noreply.github.com>
2023-09-04 22:01:32 +0300

    0.7

---
+++ Above/Assets/Scripts/Text/TextManagerTMP.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManagerTMP : MonoBehaviour
+{
+    public string language;
+    TextMeshProUGUI text;
+
+    public string textUa;
+    public string textEng;
+    public string textDen;
+    public string textFra;
+    public string textCn;
+    public string textEs;
+    public string textIta;
+    public string textPl;
+
+    void Start()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+    }
+
+    void Update()
+    {
+        language = PlayerPrefs.GetString("Language");
+
+        if (language == "" || language == "Eng")
+        {
+            text.text = textEng;
+        }
+        else if (language == "Ua")
+        {
+            text.text = textUa;
+        }
+        else if (language == "Den")
+        {
+            text.text = textDen;
+        }
+        else if (language == "Fra")
+        {
+            text.text = textFra;
+        }
+        else if (language == "Cn")
+        {
+            text.text = textCn;
+        }
+        else if (language == "Es")
+        {
+            text.text = textEs;
+        }
+        else if (language == "Ita")
+        {
+            text.text = textIta;
+        }
+        else if (language == "Pl")
+        {
+            text.text = textPl;
+        }
+    }
+}
