67abc6ed8c9c2b3148f74c344bd13fc6de8b97e4
NeuronStud1o <122213370+NeuronStud1o@users.noreply.github.com>
2024-02-09 19:47:34 +0200

    Base notifications mechanic

---
+++ Above/Assets/Scripts/MainMenu/EventPrefab.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventPrefab : MonoBehaviour
+{
+    public TextMeshProUGUI Title;
+    public TextMeshProUGUI Description;
+    public Image Icon;
+    public Button Url;
+}
